943edb0d48195184a4a47ea77ed3749f6b97fac1
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-21 13:17:30 -0300

    estruturas de repetição

---
+++ PrimeiroProjeto/estruturas_repeticao/ex1.java
@@ -0,0 +1,8 @@
+package PrimeiroProjeto.estruturas_repeticao;
+
+public class ex1 {
+    public static void main(String[] args){
+        
+    }
+    
+}
---
+++ PrimeiroProjeto/estruturas_repeticao/ex3.java
@@ -0,0 +1,20 @@
+package PrimeiroProjeto.estruturas_repeticao;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args){
+        int n, s = 0;
+        String resp;
+        Scanner teclado = new Scanner(System.in);
+        do {
+            System.out.print("Digite um número: ");
+            n = teclado.nextInt();
+            s += n;
+            System.out.print("Quer continuar? [S/N] ");
+            resp = teclado.next();
+        } while (resp.equals("S"));
+        System.out.println("A soma de todos os valores é " + s);
+    }
+    
+}
