d30a0641b4f611b31b133dedc84662031a00caba
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-21 13:08:48 -0300

    estruturas de repetição

---
+++ PrimeiroProjeto/estruturas_repeticao/ex2.java
@@ -0,0 +1,11 @@
+package PrimeiroProjeto.estruturas_repeticao;
+
+public class ex2 {
+    public static void main(String[] args){
+    int cc = 0;
+    do {
+        System.out.println("Cambalhota");
+        cc ++;      
+    } while (cc < 4);
+    }
+}
