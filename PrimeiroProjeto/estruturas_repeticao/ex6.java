200d4136cf0707c881b41f389e80240174e6d784
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-24 08:07:25 -0300

    estruturas de repetição parte 3

---
+++ PrimeiroProjeto/estruturas_repeticao/ex6.java
@@ -0,0 +1,10 @@
+package PrimeiroProjeto.estruturas_repeticao;
+
+public class ex6 {
+    public static void main(String[] args) {
+        for (int cc = 5; cc <= 15; cc+=2) {
+            System.out.println(cc);
+        }
+    }
+    
+}
