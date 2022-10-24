38fb63e307aa81e8e95e1f73815f87bf0dc73540
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-24 08:03:22 -0300

    estruturas de repetição parte 3

---
+++ PrimeiroProjeto/estruturas_repeticao/ex5.java
@@ -0,0 +1,10 @@
+package PrimeiroProjeto.estruturas_repeticao;
+
+public class ex5 {
+    public static void main(String[] args) {
+        for (int cc = 0; cc <= 3; cc++) {
+            System.out.println("Cambalhota");
+        }
+    }
+    
+}
