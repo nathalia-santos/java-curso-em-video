d93a5d82c17a6f4dc8d706f5ebd17d6e7f013813
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-25 06:42:25 -0300

    vetores

---
+++ PrimeiroProjeto/vetores/ex4.java
@@ -0,0 +1,10 @@
+package PrimeiroProjeto.vetores;
+
+public class ex4 {
+    public static void main(String[] args) {
+        int n[] = {3, 2, 8, 7, 5, 4};
+        System.out.println("Total de casa de N é " + n.length);
+        }
+    }
+    
+
