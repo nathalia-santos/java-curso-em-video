e8e6f8fa0d50f5eaa025f011120f192eccfdb1c7
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-25 08:48:56 -0300

    vetores

---
+++ PrimeiroProjeto/vetores/ex7.java
@@ -0,0 +1,14 @@
+package PrimeiroProjeto.vetores;
+
+import java.util.Arrays;
+
+public class ex7 {
+    public static void main(String[] args) {
+        double v[] = {3.5, 2.75, 9, -4.5};
+        Arrays.sort(v);
+        for (double valor: v) {
+            System.out.print(valor + " ");
+        }
+    }
+    
+}
