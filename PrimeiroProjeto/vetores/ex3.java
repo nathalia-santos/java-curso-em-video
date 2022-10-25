276e1bf0935c9354514c55d31b3df3e66a5f9206
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-25 06:34:52 -0300

    vetores

---
+++ PrimeiroProjeto/vetores/ex3.java
@@ -0,0 +1,12 @@
+package PrimeiroProjeto.vetores;
+
+public class ex3 {
+    public static void main(String[] args) {
+        int n[] = {3, 2, 8, 7, 5, 4};
+        for (int c = 0; c <= 5; c++) {
+            System.out.println("Na posição " + c + " temos o valor " + n[c]);
+        }
+    }
+    
+}
+    
