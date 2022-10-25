67b8ba74fa1a9c43bc18f20e6027221e0354f238
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-25 08:36:37 -0300

    vetores

---
+++ PrimeiroProjeto/vetores/ex6.java
@@ -0,0 +1,11 @@
+package PrimeiroProjeto.vetores;
+
+public class ex6 {
+    public static void main(String[] args) {
+        double v[] = {3.5, 2.75, 9, -4.5};
+        for (double valor: v) {
+            System.out.print(valor + " ");
+        }
+    }
+    
+}
