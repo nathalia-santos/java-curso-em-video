4fd07bd4189efd35a449cb0026da6a23ed115b61
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-10-25 06:31:35 -0300

    vetores

---
+++ PrimeiroProjeto/vetores/ex1.java
@@ -0,0 +1,13 @@
+package PrimeiroProjeto.vetores;
+
+public class ex1 {
+    
+    public static void main(String[] args) {
+        int n[] = new int[4];
+        n[0] = 2;
+        n[1] = 3;
+        n[2] = 4;
+        n[3] = 7;
+        System.out.println(n);
+    }
+}
---
+++ PrimeiroProjeto/vetores/ex2.java
@@ -0,0 +1,11 @@
+package PrimeiroProjeto.vetores;
+
+public class ex2 {
+    public static void main(String[] args) {
+        int n[] = {3, 2, 8, 7, 5, 4};
+        for (int c = 0; c <= 5; c++) {
+            System.out.println(n[c]);
+        }
+    }
+    
+}
